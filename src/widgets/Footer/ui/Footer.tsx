a543dfd8bcb910b27fe1c0247b5042668a30fa50
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-09-25 11:43:07 +0300

    fix: footer

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -12,37 +12,50 @@ export const Footer = memo((props: FooterProps) => {
   return (
     <div className="bg-gray-500 pt-10 pb-10">
       <Container>
-        <div className="grid grid-cols-5">
+        <div className="grid grid-cols-footer">
           <Flex direction="col" justify="space-between">
             <Logo size="xl" theme="light" />
-            <Text theme="white">© НАЗВАНИЕ 2023. Все права защищены</Text>
+            <Text theme="white" className="text-xs">
+              © НАЗВАНИЕ 2023. Все права защищены
+            </Text>
           </Flex>
           <Flex direction="col" gap={12}>
-            <Text theme="white">Контакты</Text>
-            <Text theme="white">Рдакция</Text>
+            <Text theme="white" className="text-sm">
+              Контакты
+            </Text>
+            <Text theme="white" className="text-sm">
+              Редакция
+            </Text>
           </Flex>
           <Flex direction="col" justify="space-between">
-            <Text theme="white">Политика конфиденциальности</Text>
-            <Text theme="white">Условия использования</Text>
-            <Text theme="white">Реклама</Text>
+            <Text theme="white" className="text-sm">
+              Политика конфиденциальности
+            </Text>
+            <Text theme="white" className="text-sm">
+              Условия использования
+            </Text>
+            <Text theme="white" className="text-sm">
+              Реклама
+            </Text>
           </Flex>
           <Flex direction="col" justify="space-between">
-            <Text theme="white">
-              По любым вопросам пишите на{" "}
+            <Text theme="white" className="text-sm">
+              По любым вопросам <br />
+              пишите на{" "}
               <span className="text-orange-500 underline">
                 privet@yandex.com
               </span>
             </Text>
-            <Flex gap={8}>
+            <Flex gap={8} align="center">
               <Tg />
-              <Text theme="white">Подписаться</Text>
+              <Text theme="white" className="text-xs">
+                Подписаться
+              </Text>
             </Flex>
           </Flex>
-          <div>
-            <Flex className="h-full" align="end">
-              <Button>Предложить новость</Button>
-            </Flex>
-          </div>
+          <Flex className="h-full text-sm w-full" justify="end" align="end">
+            <Button>Предложить новость</Button>
+          </Flex>
         </div>
       </Container>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,9 @@ const config: Config = {
       orange: {
         "500": "#FF6B00",
       },
+    },
+    gridTemplateColumns: {
+      'footer': "360px 140px 380px 285px 1fr"
     }
   },
   plugins: [],
