a030e19989a6222fa64284cc0217a990943385a3
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-09-25 12:29:38 +0300

    fix: text size

--- src/widgets/Navbar/ui/Navbar/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar/Navbar.tsx
@@ -17,18 +17,18 @@ export const Navbar = memo((props: NavbarProps) => {
       <Container className="h-[76px] flex items-center w-full">
         <Flex gap={105} align="center" className="w-full">
           <Logo />
-          <Flex className="flex-auto">
+          <Flex className="flex-auto" align="center">
             <Flex gap={20} className="flex-auto">
               {badges.map(({ icon, text }, index) => (
                 <Badge key={index} theme="gray">
-                  <Flex gap={4}>
+                  <Flex gap={4} align="center">
                     {icon}
-                    <Text>{text}</Text>
+                    <Text className="text-sm">{text}</Text>
                   </Flex>
                 </Badge>
               ))}
             </Flex>
-            <Flex gap={12}>
+            <Flex gap={12} align="center">
               <Icon icon={<Search />} />
               <Icon icon={<User />} />
             </Flex>
